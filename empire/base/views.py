92fe2e592759d8b189646fa6cf4ac5f0e0734470
knightassissin <kid30mega@gmail.com>
2025-09-03 01:22:47 +0000

    working on the structure

--- empire/base/views.py
+++ empire/base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 
+rooms = [
+    {"id": 1, "name": "let learn python!"},
+    {"id": 2, "name": "design with me"},
+    {"id": 3, "name": "Fronted develop"},
+]
+
+
 def home(request):
-    return render(request, "base/home.html")
+    return render(request, "base/home.html", {"rooms": rooms})
 
 
 def room(request):
